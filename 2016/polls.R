ed52f3e6bcd4ca5512d08d131f924824b6739398
Eric Hare <ericrhare@gmail.com>
2016-11-07 17:59:20 -0600

    Plot the polling results

--- 2016/polls.R
+++ 2016/polls.R
@@ -1,6 +1,7 @@
 library(pollstR)
 library(dplyr)
 library(tidyr)
+library(ggplot2)
 
 slug <- "2016-general-election-trump-vs-clinton"
 elec_2016_polls <- pollster_chart_data(slug)
@@ -10,3 +11,6 @@ elec_2016_polls_tidy <- elec_2016_polls %>%
     mutate(date = start_date +
                difftime(end_date, start_date, units = "days") / 2) %>%
     filter(!is.na(value))
+
+qplot(end_date, value, data = elec_2016_polls_tidy, geom = "point", colour = choice, size = I(.5)) +
+    geom_smooth(span = 0.3)
